ad2bd13e2cf90144baa97f4ea58809e30c8e86ea
Daniel Del Core <daniel.delcore@holler.com.au>
2016-05-23 16:30:29 +1000

    Refactored main setup logic + dat gui

--- src/scripts/disc.js
+++ src/scripts/disc.js
@@ -1,37 +1,53 @@
 const THREE = require('three');
 
 export default class Disc {
-    constructor() {
-        this.radius = 500;
-        const geometry = new THREE.CircleGeometry(this.radius, 100);
+    constructor(gui) {
+        this.config = {
+            size: 50.0,
+            magnitude: 100,
+            speed: 500,
+            radius: 500,
+        };
+
+        const geometry = new THREE.CircleGeometry(this.config.radius, 100);
         const material = new THREE.MeshPhongMaterial({
             color: 0xffffff,
             specular: 0xffffff,
             shininess: 30,
             shading: THREE.FlatShading,
             side: THREE.DoubleSide,
+            // wireframe: true,
         });
+
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.position.set(0, 0, 0);
         this.mesh.rotation.x = -0.3 * Math.PI;
-
         this.vCount = this.mesh.geometry.vertices.length;
         this.vStart = this.mesh.geometry.vertices[1];
         this.vEnd = this.mesh.geometry.vertices[this.vCount - 1];
         this.vCentre = this.mesh.geometry.vertices[0];
+
+        this.initGui(gui);
+    }
+
+    initGui(gui) {
+        const folder = gui.addFolder('Plane');
+        folder.add(this.config, 'size', 10, 250);
+        folder.add(this.config, 'speed', 1, 1000);
+        folder.add(this.config, 'magnitude', 10, 200);
     }
 
     // Return the ArcLength between the two vectors
     getArcLength(fromVec, toVec) {
         const angle = Math.atan2(toVec.y - fromVec.y, toVec.x - fromVec.x);
-        return (this.radius * angle);
+        return (this.config.radius * angle);
     }
 
     update(timeStamp) {
+        const { size, magnitude, speed } = this.config;
+
         for (let i = 1; i < this.vCount; i++) {
             const v = this.mesh.geometry.vertices[i];
-            const size = 50.0;
-            const magnitude = 100;
             let dist = 0;
 
             if (i === 1) {
@@ -40,7 +56,7 @@ export default class Disc {
                 dist = this.getArcLength(this.vStart, v);
             }
 
-            v.z = Math.sin(dist / -size + (timeStamp / 500)) * magnitude;
+            v.z = Math.sin(dist / -size + (timeStamp / speed)) * magnitude;
         }
 
         this.mesh.geometry.verticesNeedUpdate = true;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,84 +1,119 @@
 /* globals Detector */
+/* eslint-disable no-param-reassign */
 
 import THREE from 'three';
+import dat from 'dat-gui';
 import Disc from './disc';
+import bindAll from 'lodash/bindAll';
+
+const gui = new dat.GUI();
 
 if (!Detector.webgl) {
     Detector.addGetWebGLMessage();
     document.getElementById('container').innerHTML = '';
 }
 
-let container;
-let controls;
-let camera;
-let scene;
-let renderer;
-
-const disc = new Disc();
-
-function onResize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+function initGui(main) {
+    // Lights
+    const folder = gui.addFolder('Light');
+    folder.addColor(main.conf, 'hemisphereLightColor')
+        .onChange(c => main.hemisphereLight.color = new THREE.Color(c));
+    folder.addColor(main.conf, 'hemisphereLightColor2')
+        .onChange(c => main.hemisphereLight.groundColor = new THREE.Color(c));
+    folder.add(main.conf, 'hemisphereLightIntensity')
+        .onChange(c => main.hemisphereLight.intensity = new THREE.Color(c));
+    folder.addColor(main.conf, 'directionalLightColor')
+        .onChange(c => main.directionalLight.color = new THREE.Color(c));
+    folder.add(main.conf, 'directionalLightIntensity')
+        .onChange(c => main.directionalLight.intensity = c);
 }
 
-function initRenderer() {
-    renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.setClearColor(0x17293a);
+class Main {
+    constructor() {
+        bindAll(this,
+            'animate',
+            'onResize');
+
+        this.conf = {
+            hemisphereLightColor: 0xFF004A,
+            hemisphereLightColor2: 0xFFCD02,
+            hemisphereLightIntensity: 1,
+            directionalLightColor: 0xffffff,
+            directionalLightIntensity: 0.1,
+        };
+
+        // Renderer
+        this.renderer = new THREE.WebGLRenderer({ alpha: true });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        // this.renderer.setClearColor(0x17293a);
+
+        // Container
+        this.container = document.createElement('div');
+        document.body.appendChild(this.container);
+        this.container.appendChild(this.renderer.domElement);
+
+        // Scene
+        this.scene = new THREE.Scene();
+        window.addEventListener('resize', this.onResize);
+
+        // Camera
+        this.camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            0.01,
+            10000);
+        this.camera.position.set(0, 0, 1500);
+        this.camera.lookAt(0, 0, 0);
+
+        // Controls
+        this.controls = new THREE.OrbitControls(
+            this.camera,
+            this.renderer.domElement);
+        this.controls.userPan = false;
+        this.controls.userPanSpeed = 0.0;
+        this.controls.maxDistance = 5000.0;
+        this.controls.target.set(0, 0, 0);
+
+        // Light
+        this.directionalLight = new THREE.DirectionalLight(
+            this.conf.directionalLightColor,
+            this.conf.directionalLightIntensity);
+        this.directionalLight.position.set(-20, 20, 30);
+        this.hemisphereLight = new THREE.HemisphereLight(
+            this.conf.hemisphereLightColor,
+            this.conf.hemisphereLightColor2,
+            this.conf.hemisphereLightIntensity);
+        this.hemisphereLight.position.set(-20, 20, 30);
+
+        // Pièce de résistance
+        this.disc = new Disc(gui);
+
+        this.scene.add(
+            this.hemisphereLight,
+            this.directionalLight,
+            this.disc.mesh);
+    }
+
+    onResize() {
+        const { innerWidth, innerHeight } = window;
+        this.renderer.setSize(innerWidth, innerHeight);
+        this.camera.aspect = innerWidth / innerHeight;
+        this.camera.updateProjectionMatrix();
+    }
+
+    render(timeStamp) {
+        this.disc.update(timeStamp);
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    animate(timeStamp) {
+        requestAnimationFrame(this.animate);
+        this.render(timeStamp);
+    }
 }
 
-function initCamera() {
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 10000);
-    camera.position.set(0, 0, 1500);
-    camera.lookAt(scene.position);
-}
-
-function initControls() {
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.userPan = false;
-    controls.userPanSpeed = 0.0;
-    controls.maxDistance = 5000.0;
-    // controls.maxPolarAngle = Math.PI * 0.495;
-    controls.center.set(0, 0, 0);
-}
-
-function initLight() {
-    const hemisphereLight = new THREE.HemisphereLight(0xFF2A68, 0xFFCD02, 1);
-    hemisphereLight.position.set(-20, 20, 30);
-    scene.add(hemisphereLight);
-}
-
-function init() {
-    initRenderer();
-
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    container.appendChild(renderer.domElement);
-
-    scene = new THREE.Scene();
-    window.addEventListener('resize', onResize);
-
-    initCamera();
-    initControls();
-    initLight();
-
-    scene.add(disc.mesh);
-}
-
-function render(ts) {
-    disc.update(ts);
-    controls.update();
-    renderer.render(scene, camera);
-}
-
-function animate(timeStamp) {
-    requestAnimationFrame(animate);
-    render(timeStamp);
-}
-
-init();
-animate();
+const main = new Main();
+main.animate();
+initGui(main);
