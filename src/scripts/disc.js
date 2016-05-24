83e3ec3ac100e5bf4506f8a913f92a6131543786
Daniel Del Core <daniel.delcore@holler.com.au>
2016-05-24 10:51:09 +1000

    Simplified the math that calculates the distance between verticies

--- src/scripts/disc.js
+++ src/scripts/disc.js
@@ -3,70 +3,49 @@ const THREE = require('three');
 export default class Disc {
     constructor(gui) {
         this.config = {
-            // size: 50.0,
-            size: 72,
+            size: 100,
             magnitude: 100,
             speed: 500,
             radius: 500,
-            absolute: false,
+            segments: 100,
         };
 
-        const geometry = new THREE.CircleGeometry(this.config.radius, 100);
+        const geometry = new THREE.CircleGeometry(this.config.radius, this.config.segments);
         const material = new THREE.MeshPhongMaterial({
             color: 0xffffff,
             specular: 0xffffff,
             shininess: 30,
             shading: THREE.FlatShading,
             side: THREE.DoubleSide,
-            // wireframe: true,
         });
 
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.position.set(0, 0, 0);
         this.mesh.rotation.x = -0.3 * Math.PI;
         this.vCount = this.mesh.geometry.vertices.length;
-        this.vStart = this.mesh.geometry.vertices[1];
-        this.vEnd = this.mesh.geometry.vertices[this.vCount - 1];
-        this.vCentre = this.mesh.geometry.vertices[0];
-
         this.initGui(gui);
     }
 
     initGui(gui) {
         const folder = gui.addFolder('Plane');
-        folder.add(this.config, 'size', 10, 250);
+        folder.add(this.config, 'size', [50, 100])
+            .onChange(c => this.config.size = Number(c));
         folder.add(this.config, 'speed', 1, 1000);
-        folder.add(this.config, 'magnitude', 10, 200);
-        folder.add(this.config, 'absolute');
+        folder.add(this.config, 'magnitude', -500, 500);
     }
 
-    // Return the ArcLength between the two vectors
-    getArcLength(fromVec, toVec) {
-        const angle = Math.atan2(toVec.y - fromVec.y, toVec.x - fromVec.x);
-        return this.config.radius * angle;
+    getCircumference() {
+        return 2 * Math.PI * this.config.radius;
     }
 
     update(timeStamp) {
-        const { size, magnitude, speed } = this.config;
+        const { size, magnitude, speed, segments } = this.config;
 
         for (let i = 1; i < this.vCount; i++) {
+            const dist = (this.getCircumference() / segments) * i;
             const v = this.mesh.geometry.vertices[i];
-            let dist = 0;
-
-            if (i === 1) {
-                dist = -this.getArcLength(this.vEnd, v);
-            } else {
-                dist = this.getArcLength(this.vStart, v);
-            }
-
-            if (this.config.absolute) {
-                dist = Math.abs(dist);
-            }
-
             v.z = Math.sin(dist / -size + (timeStamp / speed)) * magnitude;
         }
-
-        this.hasLooped = true;
 
         this.mesh.geometry.verticesNeedUpdate = true;
     }
